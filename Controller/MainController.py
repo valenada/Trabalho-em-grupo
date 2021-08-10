3f8a2639d545e919a3c831beeb40413f8f834617
mateusst3 <mtex613@gmail.com>
2021-08-10 16:36:51 -0300

    adicionando model, view e controller

---
+++ Controller/MainController.py
@@ -0,0 +1,14 @@
+from Model import Login
+from View import TelaDeLogin
+
+
+class MainController:
+    telaLogin = TelaDeLogin.TelaDeLogin(str(input("Qual o nome do seu personagem? ")))
+    personagem = Login.Login(telaLogin.GetName())
+    def escolha(Login):
+        escolha = str(input("O nome do seu personagem é: " + Login.name + ", deseja alterar?").lower())
+        if (escolha == "sim"):
+            Login.name = str(input("Ok, escolha seu novo nome: "))
+        print("O nome escolhido para o seu personagém é: " + Login.name)
+
+    escolha(personagem)
---
+++ View/TelaDeLogin.py
@@ -0,0 +1,6 @@
+class TelaDeLogin:
+    def __init__(self, name):
+        self._name = name
+
+    def GetName(self):
+        return self._name
