7816bd5beba899741a092aa75b7609d129471cc0
valenada <82300750+valenada@users.noreply.github.com>
2021-08-16 17:51:55 -0300

    Update FaseController.py

--- FaseController.py
+++ FaseController.py
@@ -1,1 +1,18 @@
+class FaseController:
 
+    def tutorial(self):
+        escolha = int(input(
+            "Você entendeu? Posso repetir se quiser. Caso tenha entendido, escolha 1. Se quiser que eu repita, escolha 2."))
+
+    if escolha == 1:
+        self.escolhaUm()
+    else:
+        self.escolhaDois()
+
+    def escolhaUm(self):
+        print("Muito bem, você aprende rápido. Espero ter feito a escolha certa ao pedir sua ajuda.")
+
+    def escolhaDois(self):
+        print("Você é cauteloso, viajante. Isso é bom: sinal que você presta atenção."
+                   "Em momentos difíceis, você irá se deparar com dois caminhos a seguir."
+                   "Digite 1 ou 2 para fazer sua escolha, mas lembre-se que tudo tem consequências.")
