33f5f16c51d727be8b80c0ae0b3c49c771cd068e
valenada <82300750+valenada@users.noreply.github.com>
2021-08-16 18:03:28 -0300

    Update FaseController.py

--- FaseController.py
+++ FaseController.py
@@ -1,9 +1,9 @@
 class FaseController:
 
     def tutorial(self):
-        escolha = int(input("Você entendeu? Posso repetir se quiser. Caso tenha entendido, escolha 1. Se quiser que eu repita, escolha 2."))
+        escolhaloop = int(input("Você entendeu? Posso repetir se quiser. Caso tenha entendido, escolha 1. Se quiser que eu repita, escolha 2."))
 
-    if escolha == 1:
+    if escolhaloop == 1:
         self.escolhaUm()
     else:
         self.escolhaDois()
@@ -15,3 +15,22 @@ class FaseController:
         print("Você é cauteloso, viajante. Isso é bom: sinal que você presta atenção."
                    "Em momentos difíceis, você irá se deparar com dois caminhos a seguir."
                    "Digite 1 ou 2 para fazer sua escolha, mas lembre-se que tudo tem consequências.")
+
+    def bebida(self):
+        escolhabebida = int(input("Antes de eu te contar o que vai precisar fazer, imagino que esteja cansado, viajante."
+                                  "Por favor, escolha uma bebida. Eu pago."
+                                  "Se quiser Hidromel, escolha 1. Se quiser Sangue de Bruxa, digite 2."))
+        if escolhabebida == 1:
+            self.escolhaTres()
+        else:
+            self.escolhaQuatro()
+            
+    def escolhaTres(self):
+        print("Você recebeu HIDROMEL. Tem um gosto bem normal.")
+        print("Você é do tipo que não gosta de arriscar, não é?"
+              "Sinto informar que vai arriscar muito nessa missão, mas o pagamento vai valer a pena, eu garanto.")
+        
+    def escolhaQuatro(self):
+        print("Você recebeu uma garrafa de SANGUE DE BRUXA. É a melhor coisa que você já provou na vida!")
+        print("Hmmm... Gostei de você. Você busca aventura nas menores coisas."
+              "Agora tenho cerrteza de que você é a pessoa certa para esse serviço.")
